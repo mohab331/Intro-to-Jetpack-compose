e16fdc2389bce6ccd57c655368f342977ba466e9
mosama007 <mohab.osama@pwc.com>
2025-07-31 17:42:55 +0300

    adding UI and logic implementation for money counter

--- app/src/main/java/com/example/introtocompose/MainActivity.kt
+++ app/src/main/java/com/example/introtocompose/MainActivity.kt
@@ -4,13 +4,36 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.introtocompose.ui.theme.IntroToComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +42,85 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             IntroToComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MyApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun MyApp() {
+    var tapCount by remember {
+        mutableIntStateOf(0)
+    }
+    val money = tapCount * 10
+    Scaffold { contentPadding ->
+        Surface(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .padding(contentPadding),
+            color = Color(0xFF546E7A)
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                MoneyValue(value = money)
+                Spacer(modifier = Modifier.height(10.dp))
+                CreateCircle(tapCount, onClickCallback = {
+                    tapCount++
+                })
+                if (money > 50)
+                    Text(
+                        text = "Congratulations! You have enough money!",
+                        style = TextStyle(
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = Color.White
+                        )
+                    )
+            }
+        }
+    }
+}
+
+@Composable
+private fun MoneyValue(value: Int) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "Money: $value$",
+        color = Color.White,
+        fontSize = 24.sp,
+        fontWeight = FontWeight.Bold,
     )
+}
+
+@Composable
+private fun CreateCircle(tapCount: Int, onClickCallback: () -> Unit) {
+    Card(
+        Modifier
+            .padding(3.dp)
+            .size(150.dp)
+            .clickable(onClick = onClickCallback),
+        shape = CircleShape,
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+        colors = CardDefaults.cardColors(containerColor = Color(0xFFE0F7FA))
+    ) {
+        Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
+            Text(
+                text = "Tap $tapCount",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Black,
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun GreetingPreview() {
+fun MyAppPreview() {
     IntroToComposeTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
